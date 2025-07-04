be6b54358cabd419e79a5ad95ecf0f0a99f7ab15
Siddharth <kinthadasiddardha7@gmail.com>
2025-07-04 12:51:48 +0530

    Updated Home page

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -26,7 +26,7 @@ const Home = () => {
             Fashion is part of the daily air and it does not quite help that it
             changes all the time. Clothes have always been a marker of the era
             and we are in a revolution. Your fashion makes you been seen and
-            heard that way you are. So, celebrate the seasons new and exciting
+            heard that way you are. So, celebrate the new and exciting
             fashion in your own way.
           </p>
           <Link to="/products">
